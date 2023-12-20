31d38245bafa3e86551adbe4c450e28a67039437
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-12-20 16:16:59 +0100

    feat: authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import Sidebar from "@/components/Sidebar";
 import { ReactNode } from "react";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
@@ -17,8 +19,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={figtree.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
